86b386f48392499f34532654e059647f71d05463
irajwani <irajwani@bates.edu>
2018-03-02 15:57:30 -0500

    Add files via upload

---
+++ Den_Delivery-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Den_Delivery-Bridging-Header.h
+//  Den Delivery
+//
+//  Created by Tim on 4/8/16.
+//  Copyright © 2016 Den Delivery. All rights reserved.
+//
+
+#ifndef Den_Delivery_Bridging_Header_h
+#define Den_Delivery_Bridging_Header_h
+
+#import "ProgressHUD.h"
+
+#endif /* Den_Delivery_Bridging_Header_h */
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '9.0'
+
+target 'BDD' do
+  use_frameworks!
+
+  # Pods for BDD
+  pod 'Firebase'
+  pod 'Firebase/Messaging'
+  pod 'Firebase/Database'
+
+end
